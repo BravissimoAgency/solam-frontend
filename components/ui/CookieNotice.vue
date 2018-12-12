897bf1376eec267c9aeebfd79e6e6027bbe28e31
Henric Åkesson <henric@webitfuture.se>
2018-12-12 08:56:14 +0100

    Lagt in cookiepolicy.

--- components/ui/CookieNotice.vue
+++ components/ui/CookieNotice.vue
@@ -25,11 +25,44 @@
                     v-if="termsOpen"
                     @close="termsOpen = false"
                 >
-                    <h2>{{ termsText[0].heading }}</h2>
-                    <div
-                        class="content"
-                        v-html="termsText[0].content"
-                    >
+                    <h2>Cookie Policy</h2>
+                    <div class="content">
+                        <p>
+                            <b>Our policy on cookies</b><br>
+                            <a href="https://www.solam.com">https://www.solam.com</a>
+                            which is our public website,
+                            like many other websites, can store or retrieve information
+                            in your browser, usually in the form of cookies. This
+                            information that may concern you, your interests, or your
+                            internet device (computer, tablet, or mobile) is mainly
+                            used to make the site work to your expectations, and to
+                            give you a more personal experience of the web, as well
+                            as to store your previous choices to memory.
+                        </p>
+                        <p>
+                            <b>Cookies for performance</b><br>
+                            These cookies will help us to find out how our visitors
+                            use our website, so that we can judge how well it works
+                            and what can be improved upon. For instance, by using
+                            these cookies, we can find out which web pages are the
+                            most and least popular. They measure the number of
+                            visitors, how long they spend on our website and how
+                            they found the website in the first place. We can
+                            find out what we are doing well and what we can do
+                            better, as well as ensure that the web pages are
+                            loaded quicker and are correctly displayed. All
+                            information collected by these cookies is anonymous
+                            and not linked to any personal information about you.
+                        </p>
+                        <p>
+                            <b>Disapprove cookies</b><br>
+                            If our use of cookies presents problems for you, you
+                            can take action to prevent them from being applied.
+                            You can also change the settings in your browser to
+                            block certain types of cookies. For more information
+                            on how this works, you can look in the browser’s help
+                            information section.
+                        </p>
                     </div>
                 </AppDialogContent>
             </portal>
@@ -49,10 +82,6 @@ export default {
     data: () => ({
         active: window.localStorage.getItem('cookieInformationAccepted') === null,
         termsOpen: false,
-        termsText: [{
-            heading: 'Cookiespolicy',
-            content: 'Text för information om cookies',
-        }],
     }),
     methods: {
         close() {
