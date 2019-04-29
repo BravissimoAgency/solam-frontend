3d3bd2fb223ae79fbde386c2f1e53a5add37f67f
Henric Åkesson <henric@bravissimo.se>
2019-04-29 09:57:45 +0200

    Fått upp en lite roligare 404 sida.

---
+++ layouts/error.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="page">
+        <TopSection
+            :top-section="topSection"
+        />
+    </div>
+</template>
+
+<script>
+import TopSection from '../components/ui/TopSection.vue';
+
+export default {
+    components: {
+        TopSection,
+    },
+    head() {
+        return {
+            title: 'Page not found - Solam',
+        };
+    },
+    data: () => ({
+        topSection: {
+            heading: '404 - Page not found',
+            background_image: {
+                url: 'https://wp.solam.com/wp-content/uploads/2018/12/top-basicinformation.jpg',
+            },
+        },
+    }),
+};
+</script>
